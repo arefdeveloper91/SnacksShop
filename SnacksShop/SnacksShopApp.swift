fc0886183884feb95119fa061dd6ebe5921812b9
Aref <devfera14@gmail.com>
2024-07-12 14:09:58 -0300

    Initial Commit

---
+++ SnacksShop/SnacksShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacksShopApp.swift
+//  SnacksShop
+//
+//  Created by Aref Chucri on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SnacksShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
